9fc654c9ba72c493d9d40796e96b286498120e96
Xin Li <xili3@microsoft.com>
2022-09-04 10:53:15 -0700

    move currWeather from sidebar to index

--- app/components/SideBar.jsx
+++ app/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getCurrentDate, getCurrentWeather } from "../apis/utils";
+import { getCurrentDate } from "../apis/utils";
 import { getWeatherIcon } from "../apis/icons";
 import background from '../images/Cloud-background.png';
 import { MdLocationOn } from "react-icons/md";
@@ -7,40 +7,18 @@ import { MdLocationOn } from "react-icons/md";
 export default class SideBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            code: null, 
-            temprature: null, 
-            description: null,
-            country: null,
-            state: null,
-            city: null
-        };
-    }
-
-    componentDidMount() {
-        const {latitude, longitude} = this.props;
-        getCurrentWeather(latitude, longitude)
-            .then(currWeather => {
-                this.setState({
-                    code: currWeather.weather.code,
-                    temprature: currWeather.temp,
-                    description: currWeather.weather.description,
-                    country: currWeather.country_code,
-                    state: currWeather.state_code,
-                    city: currWeather.city_name
-                });
-            })
     }
 
     render() {
-        const {code, temprature, description, city, state, country} = this.state;
+        const { weather, temp: temprature, city_name: city, state_code: state, country_code: country } = this.props.currWeather;
+        const { code, description } = weather;
         const weatherIcon = getWeatherIcon(code);
         return (
             <div className="today">
                 <div className="search">search</div>
                 <div className="weather-image">
-                    <img src={background} alt="" className="cloud-background lightDark"/>
-                    <img src={weatherIcon} className="currWeatherIcon"/>
+                    <img src={background} alt="" className="cloud-background lightDark" />
+                    <img src={weatherIcon} className="currWeatherIcon" />
                 </div>
                 <div className="temperature center">
                     <span className="temp-number">{temprature}</span>
@@ -49,11 +27,11 @@ export default class SideBar extends React.Component {
                 <div className="description center lightDark">{description}</div>
                 <div className="date-location lightDark center">
                     <div>
-                        <span className="date">Today</span> • 
+                        <span className="date">Today</span> •
                         <span className="date">{getCurrentDate()}</span>
-                    </div> 
+                    </div>
                     <div className="center">
-                        <MdLocationOn className="location-symbol"/>
+                        <MdLocationOn className="location-symbol" />
                         <span>{`${city}, ${state}, ${country}`}</span>
                     </div>
                 </div>
--- app/index.jsx
+++ app/index.jsx
@@ -2,21 +2,37 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import { Forecasts } from "./components/Forecasts";
 import SideBar from "./components/SideBar";
+import { getCurrentWeather } from "./apis/utils";
 import './index.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            latitude: 47.7331456, 
-            longitude: -122.1951488
+            latitude: 47.7331456,
+            longitude: -122.1951488,
+            currWeather: null
         };
     }
 
+    componentDidMount() {
+        const { latitude, longitude } = this.state;
+        getCurrentWeather(latitude, longitude)
+            .then(currWeather => {
+                this.setState({
+                    currWeather
+                });
+            })
+    }
+
     render() {
+        if (!this.state.currWeather) {
+            return null;
+        }
+
         return (
             <React.Fragment>
-                <SideBar latitude={this.state.latitude} longitude={this.state.longitude}/>
+                <SideBar currWeather={this.state.currWeather} />
                 <div className="details">
                     <div className="forecast-highlights">
                         <Forecasts />
