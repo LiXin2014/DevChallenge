e5a820c200ebd26ec68ba2197457491c92e77ea1
Xin Li <xinli601@gmail.com>
2024-07-31 16:44:01 -0700

    added the play pause music button.

--- MusicPlayer/js/script.js
+++ MusicPlayer/js/script.js
@@ -1,2 +1,23 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+import songs from "./songs.js";
+
+var currentSong = songs[0];
+var audio = new Audio(currentSong.src);
+document.getElementById("pause-svg").style.display = "none";
+
+var onTogglePlay = function() {
+    if (audio.paused) {
+        audio.play();
+        document.getElementById("play-svg").style.display = "none";
+        document.getElementById("pause-svg").style.display = "inline";
+    } else {
+        audio.pause();
+        document.getElementById("pause-svg").style.display = "none";
+        document.getElementById("play-svg").style.display = "inline";
+    }
+}
+
+// add event listener to the play pause button
+var playButton = document.querySelector(".play-pause-button");
+playButton.addEventListener("click", onTogglePlay);
+
+
---
+++ MusicPlayer/js/songs.js
@@ -0,0 +1,15 @@
+var song1 = {
+    name: "Lost in the City Lights",
+    singer: "Cosmo Sheldrake",
+    src: "../songs/lost-in-city-lights-145038.mp3",
+    image: "../images/cover-1.png"
+}
+
+var song2 = {
+    name: "Forest Lullaby",
+    singer: "Lesfm",
+    src: "../songs/forest-lullaby-110624.mp3",
+    image: "../images/cover-2.png"
+}
+
+export default [song1, song2];
